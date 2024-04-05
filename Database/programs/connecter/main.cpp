e292dfb0801b6c89aa506ff96af787cdb8b174a5
Huda Ali <91668174+Huda-Cris@users.noreply.github.com>
2024-04-05 05:53:52 -0400

    Update main.cpp

--- Database/programs/connecter/main.cpp
+++ Database/programs/connecter/main.cpp
@@ -1,99 +1,90 @@
-#include <stdlib.h>
 #include <iostream>
-
+#include <stdlib.h>
 
 #include <mysql/jdbc.h>
 
-
-// using namespace sql;
 using namespace std;
 
-
-
-// Parent Class
 class Database {
 public:
-
-// Initialize Database
     Database(const string& host, const string& username, const string& password, const string& schema);
-    ~Database(); //Deconstructor
+    ~Database();
+    void connect();
+    void disconnect();
+    void executeQuery(const std::string& query);
 
-    void connect(); // child class to connect
-    void disconnect();// child class to disconnect
-    void executeQuery(const std::string& query); //like SELECT/INSERT/Mesages
-    // Other methods for database interactions
-
-private: //using Encapsulation
+private:
     string host_;
     string username_;
     string password_;
     string schema_;
     sql::Driver* driver_;
     sql::Connection* connection_;
-    // Other private members and methods as needed
 };
 
-// Initializer
 Database::Database(const string& host, const string& username, const string& password, const string& schema)
     : host_(host), username_(username), password_(password), schema_(schema), driver_(nullptr), connection_(nullptr) {}
 
-
-// Destructor
 Database::~Database() {
-    disconnect(); 
+    disconnect();
 }
 
-// Child class derived from main class., Inherites private varibales
-void Database::connect() {   
+void Database::connect() {
     try {
         driver_ = get_driver_instance();
         connection_ = driver_->connect("tcp://" + host_ + ":3306", username_, password_);
         connection_->setSchema(schema_);
-    } catch (const sql::SQLException& e) {
-        cout << "Connecting ERR: " << e.what()<<endl;
+    }
+    catch (const sql::SQLException& e) {
+        cout << "Connecting ERR: " << e.what() << endl;
     }
 }
 
-
-// IDEAS
-// inherited class with establish tables
-// Update polymorphism
-
-// Deleting Connection!!!!
 void Database::disconnect() {
     if (connection_) {
-        delete connection_; // delete the connection driver ptr
+        delete connection_;
         connection_ = nullptr;
     }
     driver_ = nullptr;
 }
 
-// To excute Queries
 void Database::executeQuery(const std::string& query) {
     if (!connection_) {
-        // Handle connection error
         cout << "Connecting inside executeQuery " << endl;
         return;
     }
     try {
         sql::Statement* stmt = connection_->createStatement();
         sql::ResultSet* res = stmt->executeQuery(query);
-        // Process the result set
+        while (res->next()) {
+            cout << "ID: " << res->getInt("id") << endl;
+            cout << "Campus Name: " << res->getString("campus_name") << endl;
+            cout << "Address: " << res->getString("address") << endl;
+            cout << "City: " << res->getString("city") << endl;
+            cout << "State: " << res->getString("state") << endl;
+            cout << "Country: " << res->getString("country") << endl;
+            cout << "Postal Code: " << res->getString("postal_code") << endl;
+            cout << "Latitude: " << res->getDouble("latitude") << endl;
+            cout << "Longitude: " << res->getDouble("longitude") << endl;
+            cout << "Description: " << res->getString("description") << endl;
+            cout << "Established Date: " << res->getString("established_date") << endl;
+            cout << endl;
+        }
         delete res;
         delete stmt;
-    } catch (const sql::SQLException& e) {
-        cout << "Handling Query ERR: " << e.what()<<endl;
+    }
+    catch (const sql::SQLException& e) {
+        cout << "Handling Query ERR: " << e.what() << endl;
     }
 }
 
 int main() {
-
-  // Creating my first object 
-    Database Huda("127.0.0.1", "root", "password", "studentmarket");
+    Database Huda("127.0.0.1", "root", "huda", "studentmarket");
     Huda.connect();
-    Huda.executeQuery("SELECT 'Hello World!' AS _message");
+    Huda.executeQuery("SELECT * FROM campuses");
     Huda.disconnect();
     return 0;
 }
+
 
 
